4ed4daad2e5db3415311e25bf06b97f728505baf
AI-Evgen <malinarchik90@gmail.com>
2025-02-07 00:44:28 +0500

    Update shapes.h

--- Task2/shapes.h
+++ Task2/shapes.h
@@ -6,3 +6,21 @@ public:
     virtual double perimeter() const = 0;
     virtual ~Shape() = default;
 };
+
+// Объявления производных классов
+class Rectangle : public Shape {
+    double width;
+    double height;
+public:
+    Rectangle(double w, double h);
+    double area() const override;
+    double perimeter() const override;
+};
+
+class Circle : public Shape {
+    double radius;
+public:
+    Circle(double r);
+    double area() const override;
+    double perimeter() const override;
+};
