9681b0f206fca77d90f829003d2160272dfad8fa
AI-Evgen <malinarchik90@gmail.com>
2025-02-07 15:23:41 +0500

    Update shapes.cpp

--- Task2/shapes.cpp
+++ Task2/shapes.cpp
@@ -1,24 +1,21 @@
 #include "shapes.h"
-#include <cmath>
 
-// Реализация Rectangle
 Rectangle::Rectangle(double w, double h) : width(w), height(h) {}
 
-double Rectangle::area() const {
-    return width * height;
+double Rectangle::area() const { 
+    return width * height; 
 }
 
-double Rectangle::perimeter() const {
-    return 2 * (width + height);
+double Rectangle::perimeter() const { 
+    return 2 * (width + height); 
 }
 
-// Реализация Circle
 Circle::Circle(double r) : radius(r) {}
 
-double Circle::area() const {
-    return M_PI * radius * radius;
+double Circle::area() const { 
+    return M_PI * radius * radius; 
 }
 
-double Circle::perimeter() const {
-    return 2 * M_PI * radius;
+double Circle::perimeter() const { 
+    return 2 * M_PI * radius; 
 }
