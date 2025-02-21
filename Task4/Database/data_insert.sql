f187e51232863c63abbe7507349f36eac4630ea7
AI-Evgen <malinarchik90@gmail.com>
2025-02-21 06:42:24 +0500

    Update data_insert.sql

--- Task4/Database/data_insert.sql
+++ Task4/Database/data_insert.sql
@@ -1,5 +1,13 @@
-FDConnection1.Params.Add('DriverID=MSSQL');
-FDConnection1.Params.Add('Database=Tourism');
-FDConnection1.Params.Add('User_Name=admin');
-FDConnection1.Params.Add('Password=admin123');
-FDConnection1.Params.Add('Server=localhost');
+USE Tourism;
+
+-- Добавление тестовых туров
+INSERT INTO Tours (Title, Price, DurationDays) VALUES
+('Тур в Париж', 1500.00, 7),
+('Отдых на Бали', 2500.00, 10),
+('Экскурсия по Риму', 1200.00, 5);
+
+-- Добавление тестовых заказов
+INSERT INTO Orders (ClientName, TourID) VALUES
+('Иван Петров', 1),
+('Мария Сидорова', 2),
+('Алексей Иванов', 3);
