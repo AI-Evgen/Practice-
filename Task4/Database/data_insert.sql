d3e81ce3c2834d1627bd51c326e93cff2b138d38
AI-Evgen <malinarchik90@gmail.com>
2025-02-21 06:21:15 +0500

    Update Database

---
+++ Task4/Database/data_insert.sql
@@ -0,0 +1,5 @@
+FDConnection1.Params.Add('DriverID=MSSQL');
+FDConnection1.Params.Add('Database=Tourism');
+FDConnection1.Params.Add('User_Name=admin');
+FDConnection1.Params.Add('Password=admin123');
+FDConnection1.Params.Add('Server=localhost');
