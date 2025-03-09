4469339687a0ff5a19bd87ee545a6ba5327212e5
AI-Evgen <malinarchik90@gmail.com>
2025-03-09 14:45:17 +0500

    Update WebModule.pas

--- Task4/DelphiApp/WebModule.pas
+++ Task4/DelphiApp/WebModule.pas
@@ -1,55 +1,48 @@
-unit WebModule;
-
-interface
-
-uses
-  System.SysUtils, System.Classes, Web.HTTPApp, FireDAC.Stan.Intf,
-  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
-  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
-  FireDAC.Phys.MSSQL, FireDAC.Phys.MSSQLDef, FireDAC.VCLUI.Wait, Data.DB,
-  FireDAC.Comp.Client, FireDAC.Comp.UI;
-
-type
-  TWebModule1 = class(TWebModule)
-    FDConnection1: TFDConnection;
-    FDQuery1: TFDQuery;
-    procedure WebModule1DefaultHandlerAction(Sender: TObject;
-      Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
-  private
-    { Private declarations }
-  public
-    { Public declarations }
-  end;
-
-var
-  WebModuleClass: TComponentClass = TWebModule1;
-
-implementation
-
-{$R *.dfm}
-
 procedure TWebModule1.WebModule1DefaultHandlerAction(Sender: TObject;
   Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+var
+  HTMLContent: TStringList;
 begin
-  // Подключение к БД
-  FDConnection1.Params.DriverID := 'MSSQL';
-  FDConnection1.Params.Database := 'Tourism';
-  FDConnection1.Params.UserName := 'admin';
-  FDConnection1.Params.Password := 'password';
-  FDConnection1.Connected := True;
+  HTMLContent := TStringList.Create;
+  try
+    try
+      // Подключение к БД через конфиг
+      FDConnection1.Params.LoadFromFile('PDConnection.config');
+      FDConnection1.Connected := True;
 
-  // Запрос данных
-  FDQuery1.SQL.Text := 'SELECT * FROM Tours';
-  FDQuery1.Open;
+      // Запрос данных
+      FDQueryTours.SQL.Text := 'SELECT * FROM Tours';
+      FDQueryTours.Open;
 
-  // Формирование HTML-ответа
-  Response.Content := 
-    '<html><body>' +
-    '<h1>Список туров</h1>' +
-    '<ul>' +
-    '<li>Тур: ' + FDQuery1.FieldByName('Title').AsString + '</li>' +
-    '</ul>' +
-    '</body></html>';
+      // Генерация HTML
+      HTMLContent.Add('<html><head><title>Туры</title></head><body>');
+      HTMLContent.Add('<h1>Список туров</h1><table border="1">');
+      HTMLContent.Add('<tr><th>Название</th><th>Цена</th><th>Длительность</th></tr>');
+
+      FDQueryTours.First;
+      while not FDQueryTours.Eof do
+      begin
+        HTMLContent.Add(Format(
+          '<tr><td>%s</td><td>%s</td><td>%d</td></tr>',
+          [
+            FDQueryTours.FieldByName('Title').AsString,
+            FDQueryTours.FieldByName('Price').AsString,
+            FDQueryTours.FieldByName('DurationDays').AsInteger
+          ]
+        ));
+        FDQueryTours.Next;
+      end;
+
+      HTMLContent.Add('</table></body></html>');
+      Response.Content := HTMLContent.Text;
+
+    except
+      on E: Exception do
+        Response.Content := 'Ошибка: ' + E.Message;
+    end;
+  finally
+    HTMLContent.Free;
+    FDQueryTours.Close;
+    FDConnection1.Connected := False;
+  end;
 end;
-
-end.
