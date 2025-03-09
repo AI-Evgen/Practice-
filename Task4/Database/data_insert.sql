e0d0cbf3a24fb91227151e3c574af6892ddf9a49
AI-Evgen <malinarchik90@gmail.com>
2025-03-09 22:40:09 +0500

    Update data_insert.sql

--- Task4/Database/data_insert.sql
+++ Task4/Database/data_insert.sql
@@ -1,13 +1,21 @@
 USE Tourism;
 
+-- Начало транзакции для атомарности
+BEGIN TRANSACTION;
+
 -- Добавление тестовых туров
-INSERT INTO Tours (Title, Price, DurationDays) VALUES
-('Тур в Париж', 1500.00, 7),
-('Отдых на Бали', 2500.00, 10),
-('Экскурсия по Риму', 1200.00, 5);
+INSERT INTO Tours (Title, Price, DurationDays) 
+VALUES 
+    ('Тур в Париж', 1500.00, 7),
+    ('Отдых на Бали', 2500.00, 10),
+    ('Экскурсия по Риму', 1200.00, 5);
 
 -- Добавление тестовых заказов
-INSERT INTO Orders (ClientName, TourID) VALUES
-('Иван Петров', 1),
-('Мария Сидорова', 2),
-('Алексей Иванов', 3);
+INSERT INTO Orders (ClientName, TourID) 
+VALUES 
+    ('Иван Петров', 1),
+    ('Мария Сидорова', 2),
+    ('Алексей Иванов', 3);
+
+-- Завершение транзакции
+COMMIT;
